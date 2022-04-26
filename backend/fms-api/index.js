9e076442d594805eec3445568409a6be09b37ce8
jet <6306021622029@fitm.kmutnb.ac.th>
2022-04-26 18:34:23 +0700

    first commit

---
+++ backend/fms-api/index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+
+app.get('/sleep', (req, res) => {
+  res.status(200).json({
+    message: 'Sleep',
+  })
+})
+
+app.get('/users', (req, res) => {
+  res.status(200).json({
+    message: 'jet',
+  })
+})
+
+app.get('/usersall', (req, res) => {
+  var mysql = require('mysql')
+  var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'jet',
+  })
+
+  con.connect(function (err) {
+    if (err) throw err
+    console.log('You are connected!')
+    var sql = 'select * from users'
+    con.query(sql, function (err, result) {
+      if (err) console.log(err)
+      console.log(result)
+    })
+    con.end()
+  })
+
+  res.status(200).json({
+    message: 'jet',
+  })
+})
+app.get('/userList', (req, res) => {
+  var userList = []
+  var mysql = require('mysql')
+  var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'fms',
+  })
+
+  con.connect(function (err) {
+    if (err) throw err
+    console.log('You are connected =================================!')
+    var sql = 'select * from users'
+    con.query(sql, function (err, result) {
+      /* if (err) console.log(err)
+      console.log(result)
+      userList = result*/
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(result)
+        console.log(result[0])
+        userList = result
+        console.log(userList)
+        res.status(200).json({
+          message: 'เรียกข้อมูลสำเร็จ',
+          data: userList,
+        })
+      }
+    })
+    con.end()
+  })
+  // console.log('-------------------------------------------------')
+  // console.log(userList)
+  // res.status(200).json({
+  //   message: 'เรียกข้อมูลสำเร็จ',
+  //   data: userList,
+  // })
+})
+app.use(express.static(path.join(__dirname, './fms-web-main/projectjs/public')))
+app.listen(3000, (err) => {
+  err
+    ? console.log('Fail to Start Server')
+    : console.log('Server start at 3000')
+})
